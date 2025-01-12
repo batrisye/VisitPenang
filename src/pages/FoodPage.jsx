f7c7eb2952643e23694a68e85f5abf0ddbcc0110
Khairina Batrisyia <khairinabatrisyia@Batrisyias-MacBook-Air.local>
2025-01-12 22:21:38 +0800

    food page commit

--- src/pages/FoodPage.jsx
+++ src/pages/FoodPage.jsx
@@ -9,6 +9,7 @@ const FoodPage = () => {
       <FoodVideo/>
        <div style={{ width: "100%", height: "3px", margin: "1rem 0", background: "linear-gradient(to right, #e82962, #1fc6c6)" }}> </div>
         <h2 style={{marginTop: "2rem", textAlign: "center",fontSize: "2rem",fontWeight: "bold"}}> Food and Beverages</h2>
+        <p>"Penang is considered a Melting pot of three different asian cuisines - Chinese , Indian and Malay. The food is a mix of all three and is a must try for all food lovers. The street food is a must try and is considered one of the best in the world. The food is a mix of all three and is a must try for all food lovers. The street food is a must try and is considered one of the best in the world. The food is a mix of all three and is a must try for all food lovers. The street food is a must try and is considered one of the best in the world."</p>
       <CardsSlider/>
       <div style={{ width: "100%", height: "3px", margin: "1rem 0", background: "linear-gradient(to right, #e82962, #1fc6c6)" }}> </div>
       <SkylineImage/>

8a386670fae996ffcf24ff4cb2984fc996bc5c4f
Sadeed-5N-100E <SF.icebergprism@gmail.com>
2025-01-12 22:01:28 +0800

    added indianfood slider to FoodPage.jsx

--- src/pages/FoodPage.jsx
+++ src/pages/FoodPage.jsx
@@ -3,12 +3,17 @@ import React from 'react'
 import FoodVideo from '../components/FoodVideo'
 import CardsSlider from '../components/CardsSlider'
 import SkylineImage from '../components/SkylineImage'
+import IndianFoodSlider from '..components/IndianFoodSlider'
 const FoodPage = () => {
   return (
     <div>
       <FoodVideo/>
        <div style={{ width: "100%", height: "3px", margin: "1rem 0", background: "linear-gradient(to right, #e82962, #1fc6c6)" }}> </div>
         <h2 style={{marginTop: "2rem", textAlign: "center",fontSize: "2rem",fontWeight: "bold"}}> Food and Beverages</h2>
+      <p> Penang is considered a Melting pot of three different asian cuisines - Chinese , Indian and Malay </p>
+      <div style={{ width: "100%", height: "3px", margin: "1rem 0", background: "linear-gradient(to right, #e82962, #1fc6c6)" }}> </div>
+      <h2 style={{marginTop: "2rem", textAlign: "center",fontSize: "2rem",fontWeight: "bold"}}> Indian Food </h2>
+      <IndianFoodSlider/>
       <CardsSlider/>
       <div style={{ width: "100%", height: "3px", margin: "1rem 0", background: "linear-gradient(to right, #e82962, #1fc6c6)" }}> </div>
       <SkylineImage/>
